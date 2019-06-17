dab51b8bf0dad4f2bff4fde37582bf2b91efd351
Carlos Paz <phoenixsampras@gmail.com>
2019-06-17 11:49:34 -0800

    as_latti fix

--- as_latti_payroll/__manifest__.py
+++ as_latti_payroll/__manifest__.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "as_latti_payroll",
+    'name': "Ahorasoft LATTI Payroll",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        LATTI Module
+===========================
+
+Custom module for LATTI""",
 
     'description': """
-        Long description of module's purpose
+LATTI Module
+===========================
+
+Custom module for LATTI
     """,
 
     'author': "Ahorasoft",
