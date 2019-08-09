f21006c9a184d946ba358d6cce45839eb67ea6d4
Carlos Paz <phoenixsampras@gmail.com>
2019-08-09 18:46:24 -0400

    as_latti_payroll v1.2.9 -Se corrige campos descuentos cuando el tipo de salud no es FONASA

--- as_latti_payroll/__manifest__.py
+++ as_latti_payroll/__manifest__.py
@@ -13,7 +13,7 @@ LATTI Module
 Custom module for LATTI
     """,
     'author': "Ahorasoft",
-    'version': '1.2.7',
+    'version': '1.2.9',
     'website': "http://www.ahorasoft.com",
     'category': 'Uncategorized',
     'depends': ['base','l10n_cl_hr'],
