f8e0925b19427827aa6b3315389cd589d0d44933
Carlos Paz <phoenixsampras@gmail.com>
2020-01-10 09:56:11 -0400

    bug helpdesk

--- as_latti_project/models/as_helpdesk.py
+++ as_latti_project/models/as_helpdesk.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models, _
 from odoo.tools.safe_eval import safe_eval
 from datetime import datetime, timedelta
 
-class HelpdeskTicket(models.Model):
+class as_HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     stage_time_ids = fields.One2many('helpdesk.ticket.stage.time','ticket_id', string='Tiempo por etapa')
@@ -15,19 +15,26 @@ class HelpdeskTicket(models.Model):
 
     @api.model
     def create(self, vals):
+        # Se llama al Super           
+        helpdesk = super(as_HelpdeskTicket, self).create(vals)
         # Se crea ticket con el color asignado a su grupo
+        vals = []
         if 'team_id' in vals:
             team_id = self.env['helpdesk.team'].search([('id','=',vals['team_id'])])
             if team_id:
-                vals.update({'color':team_id.color})
+                # vals.append({'color':team_id.color})
+                helpdesk.color = team_id.color
                 # if team_id.as_notify_to:
                 #     vals.append({'message_follower_ids': team_id.as_notify_to.ids})
         if not 'stage_id' in vals:
-            stage_obj = self.env['helpdesk.stage'].search([('sequence','=',1)])
+            stage_obj = self.env['helpdesk.stage'].search([('sequence','=',1)], limit=1)
             if stage_obj:
-                vals.update({'stage_id':stage_obj.id})
-        # Se llama al Super           
-        helpdesk = super(HelpdeskTicket, self).create(vals)
+                # vals.append({'stage_id':stage_obj.id})
+                helpdesk.stage_id = stage_obj.id
+                
+                
+        # helpdesk.write(vals)
+        
         #Se crea el record de tiempo
         helpdesk.stage_time_ids.create({
             'ticket_id': helpdesk.id,
@@ -53,7 +60,7 @@ class HelpdeskTicket(models.Model):
                 if obj_stage_time:
                     obj_stage_time.time += (datetime.now()-datetime.strptime(obj_stage_time.last_time,'%Y-%m-%d %H:%M:%S')).total_seconds()/3600
                     obj_stage_time.last_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        helpdesk = super(HelpdeskTicket, self).write(vals)
+        helpdesk = super(as_HelpdeskTicket, self).write(vals)
         return
 
 class helpdesk_ticket_stage_time(models.Model):
